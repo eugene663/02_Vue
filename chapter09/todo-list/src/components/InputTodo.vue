c19b655ce7779408f0929295e637163f49ed0daa
eugene663 <eugene663@naver.com>
2025-03-27 15:16:04 +0900

    20250327 기본 과제

---
+++ chapter09/todo-list/src/components/InputTodo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="row mb-3">
+    <div class="col">
+      <div class="input-group">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="할 일을 입력하세요"
+          v-model.trim="todo"
+          @keyup.enter="addTodoHandler"
+        />
+        <span class="btn btn-primary input-group-addon" @click="addTodoHandler">
+          추가
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputTodo',
+  data() {
+    return {
+      todo: '',
+    };
+  },
+  methods: {
+    addTodoHandler() {
+      if (this.todo.length >= 3) {
+        this.$emit('add-todo', this.todo);
+        this.todo = ''; // 입력 필드 초기화
+      }
+    },
+  },
+};
+</script>
---
+++ 과제/20250327/기본/calc-component-test/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h3>Calc</h3>
+    <Calc />
+
+    <hr />
+    <h3>Calc2</h3>
+    <Calc2 />
+
+    <hr />
+    <h3>Calc3</h3>
+    <Calc3 />
+
+    <hr />
+    <h3>Calc4</h3>
+    <Calc4 />
+
+    <hr />
+    <h3>Calc5</h3>
+    <Calc5 />
+  </div>
+</template>
+
+<script>
+import Calc from './components/Calc.vue';
+import Calc2 from './components/Calc2.vue';
+import Calc3 from './components/Calc3.vue';
+import Calc4 from './components/Calc4.vue';
+import Calc5 from './components/Calc5.vue';
+
+export default {
+  name: 'App',
+  components: { Calc, Calc2, Calc3, Calc4, Calc5 },
+};
+</script>
